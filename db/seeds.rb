1d8c5123f7de69808eca0fed0af9b6d8d286fbeb
granttnarg <grantwilliamthomas@hotmail.co.uk>
2021-05-26 00:09:35 +0200

    add real spotify data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 ## Clear DB
 puts "Cleaning DB..."
+Like.destroy_all
 User.destroy_all
 Record.destroy_all
 Artist.destroy_all
@@ -24,5 +25,38 @@ puts "creating 2 artists ..."
 record_one = Record.create!(title: "Party all the time", description: "Classic 80s pop anthem", genre: "pop", album: "How could it be", artwork_url: "", like_count: 667, artist: eddie)
 record_two = Record.create!(title: "Avril 14th", description: "Minimal Piano track with a nod to Erik Satie", genre: "classical", album: "drukqs", artwork_url: "", like_count: 998, artist: aphex_twin)
 puts "creating 2 records ..."
+
+puts "calling Spotify API for recommendations"
+service = SpotifyApiService.new
+service.authorize
+
+#Aphex Twin 
+seed_artists = ["6kBDZFXuLrZgHnvmPu9NsG"]
+seed_genres = ["electronic"]
+#Avril 14th
+seed_tracks = ["2MZSXhq4XDJWu6coGoXX1V"]
+limit = 100
+data = service.reccomendation_request(seed_artists, seed_genres, seed_tracks, limit)
+
+user = User.new(username:"me", email: "me@musikstore.de")
+user.password = "qwerty"
+user.save!
+
+
+data["tracks"].each do |track, index|
+  album_name = track["album"]["name"]
+  artist_name = track["artists"][0]["name"]
+  track_name = track["name"]
+  genre = "pop"
+  favourite_count = track["popularity"]
+  artist = Artist.create!(name: artist_name, favourite_count: favourite_count)
+  record = Record.create!(title: track_name, genre: genre, album: album_name, like_count: favourite_count / 2, artist: artist)
+  puts "#{artist_name} - #{track_name} added to DB"
+end
+
+like_one = Like.create!(record: Record.last, user: user)
+like_two = Like.create!(record: Record.first, user: user)
+
+puts "add #{limit} new artists and #{limit} new records"
 
 puts "seeding successful."
