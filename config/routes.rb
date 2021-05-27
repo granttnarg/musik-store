7ebdbb9e6a06cc073c8aa5feb1169903ba8c45f4
granttnarg <grantwilliamthomas@hotmail.co.uk>
2021-05-27 16:45:58 +0200

    set root path to api/v1/records output

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('api/v1/records')
+
   namespace :api do
     namespace :v1 do
-      resources :records, only: [ :index, :destroy ] do 
+      resources :records, only: [:index, :destroy ] do 
         resources :likes, only:[:create, :destroy]
       end
 
